cd432e4bbe50c32565da496f7c0ba36c72a7a1b8
Marcos <marcoswodbarros@outlook.com>
2023-06-26 20:19:57 -0300

    Creating Components

---
+++ barnabe/src/Components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div>
+            <header className='header'>
+                <h1>Projeto Barnabé</h1>
+            </header>
+        </div>
+    )
+}
+
+export default Header
