376af7f34ad5efd57fc5a3d20bc23f9c70592090
Marcos <marcoswodbarros@outlook.com>
2023-06-27 12:36:52 -0300

    Desktop main: Finishing

--- barnabe/src/Components/Identidade/Identidade.js
+++ barnabe/src/Components/Identidade/Identidade.js
@@ -11,12 +11,12 @@ const Identidade = () => {
                 </div>
 
                 <div className='identidade__container'>
-                    <img src="images/body-target.png" className='identidade__img--target' alt="Target Icon" />
+                    <img src="images/body-rocket.png" className='identidade__img--target' alt="Target Icon" />
                     <h2 className='identidade__h2'>Missão</h2>
                 </div>
 
                 <div className='identidade__container'>
-                    <img src="images/body-target.png" className='identidade__img--target' alt="Target Icon" />
+                    <img src="images/body-medal.png" className='identidade__img--target' alt="Target Icon" />
                     <h2 className='identidade__h2'>Valores</h2>
                 </div>
             </section>
