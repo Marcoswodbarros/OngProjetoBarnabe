165bbc20340422da46de0574468492fc9fd9bb70
Marcos <marcoswodbarros@outlook.com>
2023-06-27 15:00:29 -0300

    Desktop footer: Starting

---
+++ barnabe/src/Components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <div>
+        <footer className='footer'>
+            <img src="images/nav - logo.png" className='footer__logo' alt="Logo do Projeto Barnabé" />
+            <h2>Footer</h2>
+        </footer>
+    </div>
+  )
+}
+
+export default Footer
