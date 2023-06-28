4e326e0e7bcc54354ea24b31288519c941d1ff93
Marcos <marcoswodbarros@outlook.com>
2023-06-28 13:00:09 -0300

    Desktop footer: Refactoring menu gap

--- barnabe/src/Components/Menu/Menu.js
+++ barnabe/src/Components/Menu/Menu.js
@@ -7,10 +7,10 @@ const Menu = () => {
             <section className='menu'>
                 <h2 className='menu__h2'>Menu</h2>
                 <ul className='footer__menu--ul'>
-                    <li>Sobre nós</li>
-                    <li>Transparência</li>
-                    <li>Quero ajudar!</li>
-                    <li>Fale conosco!</li>
+                    <li className='footer__menu--li'>Sobre nós</li>
+                    <li className='footer__menu--li'>Transparência</li>
+                    <li className='footer__menu--li'>Quero ajudar!</li>
+                    <li className='footer__menu--li'>Fale conosco!</li>
                 </ul>
             </section>
         </div>
