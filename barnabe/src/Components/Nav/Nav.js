bee1730d7b9ed0450df79c9e0c151ff410ac1a5d
Marcos <marcoswodbarros@outlook.com>
2023-06-30 13:50:57 -0300

    Mobile header: Starting nav state

--- barnabe/src/Components/Nav/Nav.js
+++ barnabe/src/Components/Nav/Nav.js
@@ -1,26 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Nav.css'
 
 const Nav = () => {
 
+  const [active, setMode] = useState(false);
+  const toggleMode = () => {
+    setMode(!active);
+  }
+
   return (
     <div>
-        <nav className='nav'>
-            <img src="/images/nav - logo.png" className='nav__img' alt="Projeto Barnabé Logo" />
-            <ul className='nav__ul'>
-                <li className='nav__ul--li'>Início</li>
-                <li className='nav__ul--li'>Sobre nós</li>
-                <li className='nav__ul--li'>Transparência</li>
-                <li className='nav__ul--li'>Quero ajudar!</li>
-                <li className='nav__ul--li'>Fale conosco!</li>
-            </ul>
+      <nav className='nav'>
+        <img src="/images/nav - logo.png" className='nav__img' alt="Projeto Barnabé Logo" />
 
-            <div className="hamburger">
-              <span className='bar'></span>
-              <span className='bar'></span>
-              <span className='bar'></span>
-            </div>
-        </nav>
+        <div className={active ? 'icon icon__active' : 'icon'}>
+          <div className="hamburger hambuger__icon"></div>
+        </div>
+
+        <div className={active ? 'menu__open' : 'menu__close'}>
+          <ul className='nav__ul'>
+            <li className='nav__ul--li'>Início</li>
+            <li className='nav__ul--li'>Sobre nós</li>
+            <li className='nav__ul--li'>Transparência</li>
+            <li className='nav__ul--li'>Quero ajudar!</li>
+            <li className='nav__ul--li'>Fale conosco!</li>
+          </ul>
+        </div>
+      </nav>
     </div>
   )
 }
