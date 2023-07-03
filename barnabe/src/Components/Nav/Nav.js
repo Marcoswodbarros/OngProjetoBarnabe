6ac558b5a0074865e889612301c9514bce1578c8
Marcos <marcoswodbarros@outlook.com>
2023-07-03 20:03:59 -0300

    Mobile header: Updating css

--- barnabe/src/Components/Nav/Nav.js
+++ barnabe/src/Components/Nav/Nav.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react'
 import './Nav.css'
 import { Icon } from 'react-icons-kit'
-import {menu} from 'react-icons-kit/feather/menu'
-import {x} from 'react-icons-kit/feather/x'
+import { menu } from 'react-icons-kit/feather/menu'
+import { x } from 'react-icons-kit/feather/x'
 
 const Nav = () => {
 
@@ -14,18 +14,16 @@ const Nav = () => {
         <img src="/images/nav - logo.png" className='nav__img' alt="Projeto Barnabé Logo" />
 
         <div className='toggle__icon'>
-          <Icon icon={menu} size={28}/>
+          <Icon icon={menu} size={28} />
         </div>
 
-        <div className={active ? 'menu__open' : 'menu__close'}>
-          <ul className='nav__ul'>
-            <li className='nav__ul--li'>Início</li>
-            <li className='nav__ul--li'>Sobre nós</li>
-            <li className='nav__ul--li'>Transparência</li>
-            <li className='nav__ul--li'>Quero ajudar!</li>
-            <li className='nav__ul--li'>Fale conosco!</li>
-          </ul>
-        </div>
+        <ul className='nav__ul'>
+          <li className='nav__ul--li'>Início</li>
+          <li className='nav__ul--li'>Sobre nós</li>
+          <li className='nav__ul--li'>Transparência</li>
+          <li className='nav__ul--li'>Quero ajudar!</li>
+          <li className='nav__ul--li'>Fale conosco!</li>
+        </ul>
       </nav>
     </div>
   )
