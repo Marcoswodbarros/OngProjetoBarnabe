45bf0eefd725b23a0758e08068391cfb0da20b79
Marcos <marcoswodbarros@outlook.com>
2023-07-03 20:13:45 -0300

    Mobile header: Creating conditions

--- barnabe/src/Components/Nav/Nav.js
+++ barnabe/src/Components/Nav/Nav.js
@@ -6,24 +6,29 @@ import { x } from 'react-icons-kit/feather/x'
 
 const Nav = () => {
 
-
+  const [toggle, setToggle] = useState(false);
+  const handleToggle = () => {
+    setToggle(!toggle);
+  }
 
   return (
     <div>
       <nav className='nav'>
         <img src="/images/nav - logo.png" className='nav__img' alt="Projeto Barnabé Logo" />
 
-        <div className='toggle__icon'>
-          <Icon icon={menu} size={28} />
+        <div className='toggle__icon' onClick={handleToggle}>
+          {toggle ? <Icon icon={x} size={28} /> : <Icon icon={menu} size={28} />}
         </div>
 
-        <ul className='nav__ul'>
-          <li className='nav__ul--li'>Início</li>
-          <li className='nav__ul--li'>Sobre nós</li>
-          <li className='nav__ul--li'>Transparência</li>
-          <li className='nav__ul--li'>Quero ajudar!</li>
-          <li className='nav__ul--li'>Fale conosco!</li>
-        </ul>
+        <div className={toggle ? 'nav__ul expanded' : 'nav__ul'}>
+          <ul className='nav__ul'>
+            <li className='nav__ul--li'>Início</li>
+            <li className='nav__ul--li'>Sobre nós</li>
+            <li className='nav__ul--li'>Transparência</li>
+            <li className='nav__ul--li'>Quero ajudar!</li>
+            <li className='nav__ul--li'>Fale conosco!</li>
+          </ul>
+        </div>
       </nav>
     </div>
   )
